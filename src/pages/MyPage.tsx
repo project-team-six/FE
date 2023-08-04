b3c24f91314288df6e84101055cde0c37d5d5171
hotcream3904 <130561236+hotcream3904@users.noreply.github.com>
2023-08-04 20:55:13 +0900

    Feat:reduxSetting

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -4,53 +4,48 @@ import { useQuery } from "react-query";
 import { getMyPage } from "../api/user";
 
 const MyPage = () => {
-    const [userId, setUserId] = useState<number>(1);
-    const navigate = useNavigate();
+	const [userId, setUserId] = useState<number>(1);
+	const navigate = useNavigate();
 
-    const {
-        data: mypage,
-        isLoading,
-        error,
-    } = useQuery(["mypage", userId], () => {
-        return getMyPage(userId);
-    });
+	const {
+		data: mypage,
+		isLoading,
+		error,
+	} = useQuery(["mypage", userId], () => {
+		return getMyPage(userId);
+	});
 
-    return (
-        <>
-            <div>
-                {isLoading ? (
-                    <div>Loading...</div>
-                ) : (
-                    <div>
-                        {mypage.data.profileImageUrl ? (
-                            <img src={`${mypage.data.profileImageUrl}`} />
-                        ) : (
-                            <img
-                                src={require("../asstes/sobooni.png")}
-                                alt="기본 프로필이미지"
-                            />
-                        )}
-                        <p>{mypage.data.nickname}</p>
-                        <button onClick={() => navigate("/mypage/edit")}>
-                            회원정보 수정
-                        </button>
-                        <div>
-                            매너온도 어떻게 표시하지
-                            <div>{mypage.data.mannerTemperature}</div>
-                        </div>
-                        <div>
-                            <h2>작성글 {mypage.data.userPosts.length}</h2>
-                            <div>postList</div>
-                        </div>
-                        <div>
-                            <h2>관심글 {mypage.data.pinedPosts.length} </h2>
-                            <div>postList</div>
-                        </div>
-                    </div>
-                )}
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div>
+				{isLoading ? (
+					<div>Loading...</div>
+				) : (
+					<div>
+						{mypage.data.profileImageUrl ? (
+							<img src={`${mypage.data.profileImageUrl}`} />
+						) : (
+							<img src={require("../asstes/sobooni.png")} alt='기본 프로필이미지' />
+						)}
+						<p>{mypage.data.nickname}</p>
+						<button onClick={() => navigate("/mypage/edit")}>회원정보 수정</button>
+						<div>
+							매너온도 어떻게 표시하지
+							<div>{mypage.data.mannerTemperature}</div>
+						</div>
+						<div>
+							<h2>작성글 {mypage.data.userPosts.length}</h2>
+							<div>postList</div>
+						</div>
+						<div>
+							<h2>관심글 {mypage.data.pinedPosts.length} </h2>
+							<div>postList</div>
+						</div>
+					</div>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default MyPage;
